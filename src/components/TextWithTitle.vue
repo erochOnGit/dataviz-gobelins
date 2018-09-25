ade009d071de875ae0b9f0d33daf71dbc33ab86b
emile <emile31.roch@gmail.com>
2018-09-25 10:31:57 +0200

    Add text with title Component

---
+++ src/components/TextWithTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-with-title">
+    <h1 class="text-with-title__title">{{ title }}</h1>
+    <div class="text-with-title__content">{{ content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    title: String,
+    content: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.text-with-title {
+  &__title {
+    font-size: 16em;
+  }
+  &__content {
+    font-size: 4em;
+  }
+}
+</style>
