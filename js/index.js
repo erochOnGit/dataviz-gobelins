ab627f3891db0d02543a3800416c61a23857f930
MICHOT Nicolas <nmichot@g-a102b-20.local>
2018-09-25 17:05:57 +0200

    Update timeline 1705

--- js/index.js
+++ js/index.js
@@ -4,8 +4,33 @@ let yearSelected = 2018
 function timeline () {
     
     let container = document.querySelector('#container__timeline'),
-        ul = document.querySelector('#container__timeline__ul'),
-        li = document.querySelectorAll('.container__timeline__ul__li')
+        ul = document.querySelector('#container__timeline__ul')
+    
+    let yearInText = yearSelected
+
+    for (let i = 0; i <= 48; i++) {
+
+        // Create element html
+        let liNode = document.createElement('li');
+            liNode.setAttribute('class', 'container__timeline__ul__li')
+
+        let li = document.querySelectorAll('.container__timeline__ul__li')
+        ul.appendChild(liNode)
+        //ul.insertBefore(liNode, li.firstChild)
+
+        let spanText = document.createElement('span');
+            spanText.setAttribute('class', 'container__timeline__li__span')
+        liNode.appendChild(spanText)
+
+        let spanYear = document.createElement('span');
+            spanYear.setAttribute('class', 'container__timeline__li__year')
+        liNode.appendChild(spanYear)
+
+        //spanYear.innerHTML = yearSelected + 1;
+        spanYear.innerHTML = yearInText--;        
+    }
+
+    let li = document.querySelectorAll('.container__timeline__ul__li')
 
     function changeActiveClass() {
         // Init active
@@ -20,7 +45,6 @@ function timeline () {
     }
 
     changeActiveClass()
-    
 
     // Set active in click
     for (let i = 0; i < li.length; i++) {
@@ -37,15 +61,25 @@ function timeline () {
     }
 
     window.addEventListener('wheel', (e) => {
-
+        
         let firstyearOfList = document.querySelector('.container__timeline__ul__li:first-child').lastElementChild.innerHTML,
             lastYearOfList = document.querySelector('.container__timeline__ul__li:last-child').lastElementChild.innerHTML
 
-        if (yearSelected > firstyearOfList++) {
+        if (yearSelected == firstyearOfList--) {
+
+            console.log('++')
+            
             yearSelected = firstyearOfList
-        } else if (yearSelected < lastYearOfList--) {
+            return false
+
+        } else if (yearSelected == lastYearOfList++) {
+            
             yearSelected = lastYearOfList
+            return false
+            
         }
+
+        console.log(yearSelected)
 
         if (Math.sign(e.wheelDeltaY) == 1) {
             yearSelected++
@@ -56,5 +90,4 @@ function timeline () {
         changeActiveClass()
     })
 }
-
 timeline()
